af4061d675f5024ad8977e9f9ba289c5147064b4
Joshua Sajeev <joshuasajeevnv@gmail.com>
2025-02-07 18:53:23 +0530

    Tailwind Connected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,74 +1,15 @@
 import { useState } from "react";
-import "./App.css";
-
+import InputComponent from "./components/InputComponent.jsx"
 function App() {
-    const [title, setTitle] = useState("");
-    const [amount, setAmount] = useState("");
-    const [message, setMessage] = useState("");
-
-    const fetchData = () => {
-        fetch("http://localhost:8080/")
-            .then(response => response.text())
-            .then(data => setMessage(data))
-            .catch(error => console.error("Error fetching data:", error));
-    };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        const data = { title, amount: parseFloat(amount) };
-
-        try {
-            const response = await fetch("http://localhost:8080/api/submit", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-
-            const result = await response.json();
-            setMessage(result.message); // Update message state
-        } catch (error) {
-            console.error("Error submitting data:", error);
-            setMessage("Error submitting data.");
-        }
-    };
 
     return (
-        <div>
-            <button onClick={fetchData}>Click to fetch from Go server</button>
-
-            {message && (
-                <div>
-                    <h2>Server Response:</h2>
-                    <p>{message}</p>
-                </div>
-            )}
-
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Title:
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </label>
-                <label>
-                    Enter your amount:
-                    <input
-                        type="number"
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                    />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+        <div className="">
+            <div className="text-center">
+                <label>Cloud Spend</label>
+            </div>
+            <div className="border-3 rounded-lg border-indigo-700 p-5">
+                <InputComponent/>
+            </div>
         </div>
     );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <div className="flex h-screen bg-[#FAF5EF] items-center justify-center">
+            <div className="border-5 border-purple-500  p-5 rounded-lg">
+                <App />
+            </div>
+        </div>
+    </StrictMode>,
 )
